bdc6a90a892f927f75d4fcd52df436d901891400
MelsenC <melsen.carlsburg@gmail.com>
2017-05-22 19:51:31 +0000

    created Work_Order model and edited database migration

--- db/migrate/20170519200926_create_work_orders.rb
+++ db/migrate/20170519200926_create_work_orders.rb
@@ -1,7 +1,6 @@
 class CreateWorkOrders < ActiveRecord::Migration[5.0]
   def change
     create_table :work_orders do |t|
-      t.datetime :date_created
       t.datetime :date_needed
       t.string :company
       t.string :cust_name
