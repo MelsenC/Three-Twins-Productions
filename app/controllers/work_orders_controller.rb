c64d6408ecfb93bcece65405a42ba7ce50957983
MelsenC <melsen.carlsburg@gmail.com>
2017-05-22 23:32:34 +0000

    adjusted work order form, added links and adjusted WorkOrdersController to create new database entries

--- app/controllers/work_orders_controller.rb
+++ app/controllers/work_orders_controller.rb
@@ -6,4 +6,17 @@ class WorkOrdersController < ApplicationController
   def new
     @work_order = WorkOrder.new
   end
+
+  def create
+    WorkOrder.create(work_order_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def work_order_params
+    params.require(:work_order).permit(:date_needed, :company, :cust_name, :cust_po, :description)
+  end
+
 end
+
